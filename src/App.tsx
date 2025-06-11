bd38b479d1badff9a9ed9b5e8fc0049c9e70d1d8
hakim-az <hakimazzaz93@gmail.com>
2025-06-11 16:45:02 +0200

    feat: adding the essentials starter  packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 import { motion } from 'motion/react'
+import { ToastContainer, toast } from 'react-toastify'
 
 export default function App() {
   const [counter, setCounter] = useState(0)
+  const notify = () => toast('Wow so easy!')
   return (
     <motion.div className="flex min-h-svh flex-col items-center justify-center">
       <motion.div animate={{ rotate: 360 }}>
-        <Button onClick={() => setCounter(counter + 1)}>
+        <Button
+          onClick={() => {
+            setCounter(counter + 1)
+            notify()
+          }}>
           Click me : {counter}
         </Button>
       </motion.div>
+      <ToastContainer />
     </motion.div>
   )
 }
