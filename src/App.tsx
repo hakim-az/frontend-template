e5e4fc103c1b40339633261a3c2da964c4a8f958
hakim-az <hakimazzaz93@gmail.com>
2025-06-11 16:10:16 +0200

    feat: adding import alias, tailwind css, shadcn ui and motion react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { motion } from 'motion/react'
+
 export default function App() {
+  const [counter, setCounter] = useState(0)
   return (
-    <div>
-      <h1>Frontend Template</h1>
-    </div>
+    <motion.div className="flex min-h-svh flex-col items-center justify-center">
+      <motion.div animate={{ rotate: 360 }}>
+        <Button onClick={() => setCounter(counter + 1)}>
+          Click me : {counter}
+        </Button>
+      </motion.div>
+    </motion.div>
   )
 }
