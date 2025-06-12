c3e27608aecdfa15d8eb7704bf23d04c41c04660
hakim-az <hakimazzaz93@gmail.com>
2025-06-12 15:36:38 +0200

    fix: fixing test app component issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import { describe, test, expect } from 'vitest'
 import { render } from '@testing-library/react'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('<App />', () => {
   test('App mounts properly', () => {
-    const wrapper = render(<App />)
+    const wrapper = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
     expect(wrapper).toBeTruthy()
   })
 })
